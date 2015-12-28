d9ee786e0c7a13430319f7e931f6e2bc361e2f14
honmio <330516202@qq.com>
2015-12-28 13:59:25 +0800

    Quick Sort Case

---
+++ MagicMath/MagicMath/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicMath
+{
+				/// <summary>
+				/// quick sort
+				/// </summary>
+				public static class QuickSort
+				{
+								public static void Sort(List<int> ints, int left, int right)
+								{
+												if (left > right)
+																return;
+
+												int temp = ints[left];
+												int x = left;
+												int y = right;
+												while (x < y)
+												{
+																while (x < y && ints[y] >= temp)
+																				y--;
+
+																while (x < y && ints[x] <= temp)
+																				x++;
+
+																if (x < y)
+																{
+																				int t = ints[x];
+																				ints[x] = ints[y];
+																				ints[y] = t;
+																}
+												}
+
+												ints[left] = ints[x];
+												ints[x] = temp;
+
+												Sort(ints, left, x - 1);
+												Sort(ints, x + 1, right);
+												return;
+								}
+				}
+}
